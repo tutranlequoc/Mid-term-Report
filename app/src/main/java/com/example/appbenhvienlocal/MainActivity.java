a2c01fefc065aa86562a26641ba2d6c5a25e8cdd
Nguyen Van Cuong <bacnd19411@st.uel.edu.vn>
2021-10-30 21:10:15 +0700

    Add branch

--- app/src/main/java/com/example/appbenhvienlocal/MainActivity.java
+++ app/src/main/java/com/example/appbenhvienlocal/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,6 +30,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
+
     GridView gvFunction;
     ArrayList<Function> listFunction;
     CustomAdapter adapter;
@@ -42,6 +47,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         linkViews();
         initData();
         loadData();
@@ -88,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int itemId = item.getItemId();
         if(itemId==R.id.nav_paper)
         {
-            
+
         }
         else if (itemId==R.id.nav_phieuKham) {
         }
---
+++ app/src/main/java/com/example/models/Region.java
@@ -0,0 +1,17 @@
+package com.example.models;
+
+public class Region {
+    private int regionThumb;
+
+    public Region(int regionThumb) {
+        this.regionThumb = regionThumb;
+    }
+
+    public int getRegionThumb() {
+        return regionThumb;
+    }
+
+    public void setRegionThumb(int regionThumb) {
+        this.regionThumb = regionThumb;
+    }
+}
