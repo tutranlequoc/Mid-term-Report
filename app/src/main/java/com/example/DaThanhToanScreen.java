25652bfc76b120056c15a333dc82f39864185234
Nguyen Van Cuong <bacnd19411@st.uel.edu.vn>
2021-10-31 19:30:08 +0700

    Add các màn hình còn lại

---
+++ app/src/main/java/com/example/DaThanhToanScreen.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appbenhvienlocal.R;
+
+public class DaThanhToanScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_da_thanh_toan_screen);
+    }
+}
