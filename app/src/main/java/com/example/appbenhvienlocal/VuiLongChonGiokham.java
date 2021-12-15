612176e3888cc3192c6357aacfb74453c4939521
LHG-4301 <!@4301lG*_2001>
2021-12-15 21:39:32 +0700

    Sửa backend

--- app/src/main/java/com/example/appbenhvienlocal/VuiLongChonGiokham.java
+++ app/src/main/java/com/example/appbenhvienlocal/VuiLongChonGiokham.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -17,6 +18,7 @@ import com.example.ultis.Constant;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class VuiLongChonGiokham extends AppCompatActivity {
@@ -27,8 +29,8 @@ public class VuiLongChonGiokham extends AppCompatActivity {
     ArrayList<ThongTin> ketQuaTraVe;
     ArrayList<ThoiGianKham> timesMor, timesEve ;
     GridView gvMor, gvEve;
-    Intent intent;
     GioKhamAdapter adapterMor, adapterEve;
+    Intent intent;
     int index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +38,8 @@ public class VuiLongChonGiokham extends AppCompatActivity {
         setContentView(R.layout.activity_vui_long_chon_giokham);
         linkViews();
         loadApdater();
-        addEvents();
         getData();
+        addEvents();
     }
 
     private void loadApdater() {
@@ -63,6 +65,11 @@ public class VuiLongChonGiokham extends AppCompatActivity {
         index = intent.getIntExtra(Constant.INDEX_THONGTIN, 0);
     }
 
+    private String timeFromLocalTime(LocalTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return time.format(formatter);
+    }
+
     private void addEvents() {
         btnGK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,12 +81,40 @@ public class VuiLongChonGiokham extends AppCompatActivity {
                 }else {
                     Intent backToFirstScreen = new Intent(VuiLongChonGiokham.this, ChonThongTinKham.class);
                     backToFirstScreen.putExtra(Constant.THONGTIN, ketQuaTraVe);
-                    backToFirstScreen.putExtra(Constant.INDEX_THONGTIN, index - 1);
+                    backToFirstScreen.putExtra(Constant.INDEX_THONGTIN, index + 1);
                     startActivity(backToFirstScreen);
                 }
 
             }
         });
+
+       gvMor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                 ThoiGianKham time = (ThoiGianKham) adapterMor.getItem(i);
+                 LocalTime startTime = time.getThoiGianBĐ();
+                 LocalTime endTime = time.getThoiGianKT();
+                 ketQuaTraVe.get(index).setResult(timeFromLocalTime(startTime) + " - " + timeFromLocalTime(endTime));
+                 intent = new Intent(VuiLongChonGiokham.this, ChonThongTinKham.class);
+                 intent.putExtra(Constant.THONGTIN, ketQuaTraVe);
+                 intent.putExtra(Constant.INDEX_THONGTIN, index + 1);
+                 startActivity(intent);
+           }
+       });
+
+       gvEve.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               ThoiGianKham time = (ThoiGianKham) adapterEve.getItem(i);
+               LocalTime startTime = time.getThoiGianBĐ();
+               LocalTime endTime = time.getThoiGianKT();
+               ketQuaTraVe.get(index).setResult(timeFromLocalTime(startTime) + " - " + timeFromLocalTime(endTime));
+               intent = new Intent(VuiLongChonGiokham.this, ChonThongTinKham.class);
+               intent.putExtra(Constant.THONGTIN, ketQuaTraVe);
+               intent.putExtra(Constant.INDEX_THONGTIN, index + 1);
+               startActivity(intent);
+           }
+       });
     }
 
     private void linkViews() {
