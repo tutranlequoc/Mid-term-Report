59b4e367cbec48c7fca2be8a2825030c4f76026a
Tu Tran Le Quoc <tutlq19411@st.uel.edu.vn>
2021-12-24 21:22:35 +0700

    update layout

--- app/src/main/java/com/example/appbenhvienlocal/fragment/forgetID.java
+++ app/src/main/java/com/example/appbenhvienlocal/fragment/forgetID.java
@@ -4,17 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.adapter.CategoryAdapter;
 import com.example.appbenhvienlocal.R;
 
 public class forgetID extends Fragment {
+    CategoryAdapter genderAdapter, provinceAdapter;
+    Spinner spnGender, spnProvince;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        spnGender = container.findViewById(R.id.spn_gioitinh_quenMaHS);
+        spnProvince = container.findViewById(R.id.spn_tinhTP_quenMaHS);
+        //genderAdapter = new CategoryAdapter(this, R.layout.item_selected_quen_ma_hs, getListGender());
         return inflater.inflate(R.layout.screen_quenmaso,container,false);
+        
+    }
+
+    private Object getListGender() {
+        return true;
     }
 }
--- app/src/main/java/com/example/appbenhvienlocal/screen_phuongthucthanhtoan.java
+++ app/src/main/java/com/example/appbenhvienlocal/screen_phuongthucthanhtoan.java
@@ -4,29 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+
+import com.example.adapter.Bank;
+import com.example.adapter.BankAdapter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class screen_phuongthucthanhtoan extends AppCompatActivity {
     Button btnthanhToanATM, btnthanhToanMomo,btnBack;
+    ViewGroup linearMethod;
+    GridView gvBank;
+    boolean expandable;
+    BankAdapter bankAdapter;
+    ArrayList<Bank> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_phuongthucthanhtoan);
         linkViews();
+        initData();
         addEvents();
+    }
+
+    private void initData() {
+        arrayList = new ArrayList<>();
+        arrayList.add(new Bank("Agribank",R.drawable.agribank));
+        arrayList.add(new Bank("Techcombank",R.drawable.techcombank));
+        bankAdapter = new BankAdapter(this,R.layout.bank_list,arrayList);
+        gvBank.setAdapter(bankAdapter);
     }
 
     private void addEvents() {
         btnthanhToanATM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(screen_phuongthucthanhtoan.this, screen_thongtinthanhtoan.class);
-                Bundle bundle1 = new Bundle();
-                bundle1.putString("atm","Bạn đang thực hiện thanh toán bằng Thanh toán bằng thẻ ATM nội địa/Internet Banking");
-                intent1.putExtra("method1",bundle1);
-                startActivity(intent1);
+                TransitionManager.beginDelayedTransition(linearMethod);
+                expandable = !expandable;
+                gvBank.setVisibility(expandable ? View.VISIBLE : View.GONE);
+                gvBank.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent(screen_phuongthucthanhtoan.this, screen_thongtinthanhtoan.class);
+                        //put extra
+                        startActivity(intent);
+                    }
+                });
             }
 
         });
@@ -53,5 +83,7 @@ public class screen_phuongthucthanhtoan extends AppCompatActivity {
         btnthanhToanATM = findViewById(R.id.btnThanhToanATM);
         btnthanhToanMomo = findViewById(R.id.btnThanhToanMomo);
         btnBack = findViewById(R.id.btnBackPhuongThuc);
+        linearMethod = findViewById(R.id.linearMethods);
+        gvBank = findViewById(R.id.gvBanks);
     }
 }
